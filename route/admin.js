88d3c42f0d43a1fa6810bee16beeba22355a5464
kimbro97 <rlagudwog@gmail.com>
2022-02-15 01:30:49 +0900

    Add route

--- route/admin.js
+++ route/admin.js
@@ -4,5 +4,8 @@ const controllers = require('../controllers')
 router.get('/order', controllers.Order)
 router.patch('/permission', controllers.permission)
 router.patch('/rejection', controllers.rejection)
-router.get('/defaulter', controllers.defaulter)
+router.get('/defaulter', controllers.getdefaulter)
+router.patch('/defaulter', controllers.postdefaulter)
+router.patch('/completion', controllers.completion)
+
 module.exports = router
--- route/order.js
+++ route/order.js
@@ -1,6 +1,8 @@
 const router = require('express').Router()
 const controllers = require('../controllers')
 
-router.post('/', controllers.order)
+router.get('/', controllers.getOrder)
+router.post('/', controllers.postOrder)
+router.delete('/', controllers.deleteOrder)
 
 module.exports = router
